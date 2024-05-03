d6188f655f8744f9fd1670a6ce6f97022b5533f7
Jean Luis <jean.nafre@gmail.com>
2024-05-02 20:34:13 -0400

    Login - Se agrega form para login

---
+++ src/pages/test.tsx
@@ -0,0 +1,13 @@
+import { NextPage } from "next";
+import NavBar from "src/components/nav-bar";
+
+const Page : NextPage = () => {
+
+    return (
+        <>
+            <NavBar />
+        </>
+    ) ;
+} 
+
+export default Page;
